934e7e9273ac63a9fde1034a2844d05bf6d0a30e
Rashid Obaidi <rashid.obaidi2003@gmail.com>
2018-09-14 03:26:25 +0000

    submit expence and submit incom web services

--- web/admin.py
+++ web/admin.py
@@ -4,11 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Expense
 from .models import Income
+from .models import Token
+
 
 
 # Register your models here.
 
 admin.site.register(Expense)
 admin.site.register(Income)
+admin.site.register(Token)
 
 	
